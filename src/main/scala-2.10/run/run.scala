642caf0472ccd1a8fd53efd977a3f4a0ed2ec643
hzliang <huzuoliang1990@163.com>
2016-01-23 17:04:43 +0800

    添加KMeans聚类算法

--- src/main/scala-2.10/run/run.scala
+++ src/main/scala-2.10/run/run.scala
@@ -1,7 +1,9 @@
 package run
 
+import breeze.linalg.{DenseVector, DenseMatrix}
 import kmeans.KM
 import org.apache.spark.mllib.linalg.Vectors
+import pc.SKPC
 import spark.SparkObj
 
 /**
@@ -11,9 +13,17 @@ object Run {
 
     def main(args: Array[String]) {
         val master = "hdfs://192.168.1.121:9000"
-        val input = master + "/hzl/input/cros3.csv"
-        val data = SparkObj.ctx.textFile(input).map(s => Vectors.dense(s.split(',').map(_.toDouble))).cache()
-        val clus = KM.km(data)
-        val clusterRes = clus.predict(data).fo
+        val input = master + "/hzl/input/cros3.csv" //输入文件
+        //将输入每一行数据转成向量Dense(x,y)
+        val dataRDD = SparkObj.ctx.textFile(input).map(s => Vectors.dense(s.split(',').map(_.toDouble))).cache()
+        val clus = KM.km(dataRDD)
+        clus.predict(dataRDD).zip(dataRDD).groupByKey.map(v => {
+            val gi = v._2.toArray
+            val data = DenseMatrix.zeros[Double](gi.length, 2)
+            for (i <- 0 until gi.length) {
+                data(i, ::) := DenseVector[Double](gi(i).toArray).t
+            }
+            SKPC.extractPC(data)
+        })
     }
 }
