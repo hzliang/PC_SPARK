bb7dbbe0f65cb7434dd3b8cec007e8f30311a901
hzliang <huzuoliang1990@163.com>
2016-01-24 14:23:32 +0800

    完成主函数

--- src/main/scala-2.10/run/Run.scala
+++ src/main/scala-2.10/run/Run.scala
@@ -1,25 +1,24 @@
 package run
 
-import breeze.linalg.{DenseVector, DenseMatrix}
+import breeze.linalg.{DenseMatrix, DenseVector}
 import kmeans.KM
 import org.apache.spark.mllib.linalg.Vectors
 import pc.{PCUtil, SKPC}
 import spark.SparkObj
-import util.MatrixUtil
 
 /**
-  * Created by ad on 2016/1/23.
-  */
+ * Created by ad on 2016/1/23.
+ */
 object Run {
 
     def main(args: Array[String]) {
         val master = "hdfs://192.168.1.121:9000"
-        val input = master + "/hzl/input/cros3.csv" //输入文件
+        val input = master + args(0) //"/hzl/input/cros3.csv" //输入文件
         //将输入每一行数据转成向量Dense(x,y)
         val dataRDD = SparkObj.ctx.textFile(input).map(s => {
                 Vectors.dense(s.split(',').map(_.toDouble))
             }).cache()
-        val clusRDD =  KM.km(dataRDD).predict(dataRDD).zip(dataRDD)
+        val clusRDD = KM.km(dataRDD).predict(dataRDD).zip(dataRDD)
         val allLines = clusRDD.groupByKey.map(v => {
             val data = v._2.toArray
             val dataMat = DenseMatrix.zeros[Double](data.length, 2)
