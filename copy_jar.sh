d036f49986f0f71b899f4357716ccc9317e190ca
hzliang <huzuoliang1990@163.com>
2016-01-26 21:11:09 +0800

    添加远程拷贝脚本

---
+++ copy_jar.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+zip -d /home/hy/PC_SPARK/out/artifacts/pc_spark_jar/pc_spark.jar META-INF/*.RSA META-INF/*.DSA META-INF/*.SF
+scp /home/hy/PC_SPARK/out/artifacts/pc_spark_jar/pc_spark.jar root@192.168.1.121:./
