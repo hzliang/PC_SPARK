fc81a4912ec3b5316b440932535927f1dab66a69
hzliang <huzuoliang1990@163.com>
2016-01-29 15:37:38 +0800

    添加测试函数  测试平行

--- src/test/scala-2.10/Test.scala
+++ src/test/scala-2.10/Test.scala
@@ -1,4 +1,4 @@
-import breeze.linalg.{DenseVector, DenseMatrix}
+import breeze.linalg.{Axis, DenseVector, DenseMatrix}
 import pc.PCUtil
 import util.MatrixUtil
 
@@ -7,18 +7,33 @@ import util.MatrixUtil
   */
 object Test {
   def main(args: Array[String]) {
-    //    var oldLink = new Array[Int](2)
-    //    oldLink = oldLink.map(_ + 1)
-    //    println(oldLink.mkString(","))
-    //    val LinkD = DenseVector[Int](2, 3, 4, 5, 1, 9)
-    //    val re = PCUtil.link(oldLink, LinkD)
-    //    val m1 = DenseMatrix.zeros[Double](2, 2)
-    //    val m2 = DenseMatrix.zeros[Double](2, 2)
-    //    print(MatrixUtil.matrix2String(m2, "matlab"))
-    val v1 = DenseVector[Double](2, 2)
-    val v2 = DenseVector[Double](1, 1)
-    val m = new DenseMatrix[Double](2, 2, Array(-1.0, -0.0,1.0,1.0))
-    val dis = PCUtil.line2LineProDist(v1, v2, m)
-    println(dis)
+    testPllFun
   }
+
+  def testPllFun() = {
+    val m = DenseMatrix.zeros[Double](2, 6)
+    m(::, 0) := DenseVector[Double](1.0, 1.0)
+    m(::, 1) := DenseVector[Double](2.0, 2.0)
+    m(::, 2) := DenseVector[Double](0.5, 0.5)
+    m(::, 3) := DenseVector[Double](0.8, 0.8)
+    m(::, 4) := DenseVector[Double](0.51, 0.51)
+    m(::, 5) := DenseVector[Double](0.81, 0.81)
+
+
+    val res = PCUtil.filterPllLines(m, 10.0)
+    println(res)
+
+  }
+
+  def testIf() = {
+    val m = DenseMatrix.zeros[Double](2, 4)
+    m(::, 0) := DenseVector[Double](0.0, 0.0)
+    m(::, 1) := DenseVector[Double](1.0, 1.0)
+    m(::, 2) := DenseVector[Double](0.5, 0.5)
+    m(::, 3) := DenseVector[Double](0.8, 0.8)
+
+    println(m.delete(1, axis = Axis._1))
+  }
+
+
 }
