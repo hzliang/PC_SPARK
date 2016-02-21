f7d53cfd85da8f3985895c9afd99192947274586
hzliang <huzuoliang1990@163.com>
2016-02-21 19:09:27 +0800

    修改主函数

--- src/main/scala-2.10/run/Run.scala
+++ src/main/scala-2.10/run/Run.scala
@@ -1,45 +1,55 @@
-//package run
-//
-//import breeze.linalg.{DenseMatrix, DenseVector}
-//import kmeans.KM
-//import org.apache.spark.mllib.linalg.Vectors
-//import pc.{PCUtil, SKPC}
-//import spark.{StringAccumulableParam, SparkObj}
-//import util.{MatrixUtil, VectorUtil}
-//
-///**
-//  * Created by ad on 2016/1/23.
-//  */
-//object Run {
-//
-//  def main(args: Array[String]) {
-//    val master = "hdfs://192.168.1.121:9000"
-//    val input = master + args(0) //"/hzl/input/cros3.csv" //输入文件
-//    //将输入每一行数据转成向量Dense(x,y)
-//    val dataRDD = SparkObj.ctx.textFile(input).map(s => {
-//        Vectors.dense(s.split(',').map(_.toDouble))
-//      }).cache()
-//    val clusRDD = KM.kmeans(dataRDD).predict(dataRDD).zip(dataRDD).persist()
-//
-//    val acc = SparkObj.ctx.accumulator("", "stringAcc")(StringAccumulableParam)
-//    clusRDD.foreach(v => acc += v._1 + "," + v._2.toArray.mkString(","))
-//    val allLines = clusRDD.groupByKey.map(v => {
-//      val data = v._2.toArray
-//      val dataMat = DenseMatrix.zeros[Double](data.length, 2)
-//      for (i <- 0 until data.length) {
-//        dataMat(i, ::) := new DenseVector[Double](data(i).toArray).t
-//      }
-//      //转成字符串返回，带固定结果
-//      //shffle会需大量时间
-//      MatrixUtil.matrix2String(SKPC.extractPC(dataMat), "reduce")
-//    }).reduce(MatrixUtil.matrixPlus2String(_, _))
-//    //输出结果
-//    println("the cluster result is:")
-//    println(acc.value)
-//    println("The original lines are:")
-//    println(allLines)
-//    println("The linked lines are:")
-//    println(VectorUtil.vector2String(
-//      PCUtil.linkLines(MatrixUtil.matrixStr2Matrix(allLines))))
-//  }
-//}
+package run
+
+import breeze.linalg.{DenseMatrix, DenseVector}
+import kmeans.{ConfigKM, KM}
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.rdd.RDD
+import pc.{PCUtil, SKPC}
+import spark.{StringAccumulableParam, SparkObj}
+import util.{MatrixUtil, VectorUtil}
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  * Created by ad on 2016/1/23.
+  */
+object Run {
+
+  def main(args: Array[String]) {
+    val master = "hdfs://192.168.1.121:9000"
+    val input = master + args(0) //"/hzl/input/cros3.csv" //输入文件
+    //将输入每一行数据转成向量Dense(x,y)
+    val dataRDD = SparkObj.ctx.textFile(input).map(s => {
+        Vectors.dense(s.split(',').map(_.toDouble))
+      }).cache()
+    //    val clusRDD = KM.kmeans(dataRDD).predict(dataRDD).zip(dataRDD).persist()
+    ConfigKM.totalDataCount = dataRDD.count()
+    //    val clus = KMeans.train(parsedData, ConfigKM.classCount,
+    //      ConfigKM.itersTimes, ConfigKM.reRunTimes).predict(parsedData).
+    //      map((_, 1)).reduceByKey(_ + _)
+    val accum = SparkObj.ctx.accumulator(ConfigKM.classCount, "My Accumulator")
+    val rddArray = new ArrayBuffer[RDD[(Int, Vector)]]()
+    KM.kmeans(dataRDD, ConfigKM.classCount, accum, accum.value, rddArray, false)
+    val clus = rddArray.reduce(_ union _)
+    //    val acc = SparkObj.ctx.accumulator("", "stringAcc")(StringAccumulableParam)
+    //    clusRDD.foreach(v => acc += v._1 + "," + v._2.toArray.mkString(","))
+    val allLines = clus.groupByKey.map(v => {
+      val data = v._2.toArray
+      val dataMat = DenseMatrix.zeros[Double](data.length, 2)
+      for (i <- 0 until data.length) {
+        dataMat(i, ::) := new DenseVector[Double](data(i).toArray).t
+      }
+      //转成字符串返回，带固定结果
+      //shffle会需大量时间
+      MatrixUtil.matrix2String(SKPC.extractPC(dataMat), "reduce")
+    }).reduce(MatrixUtil.matrixPlus2String(_, _))
+    //    //输出结果
+    //    println("the cluster result is:")
+    //    println(acc.value)
+    println("The original lines are:")
+    println(allLines)
+    println("The linked lines are:")
+    println(VectorUtil.vector2String(
+      PCUtil.linkLines(MatrixUtil.matrixStr2Matrix(allLines))))
+  }
+}
