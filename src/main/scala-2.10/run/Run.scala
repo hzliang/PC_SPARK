fc9bc007460d8543c4e483801b267e9b847604cd
hzliang <huzuoliang1990@163.com>
2016-03-12 15:57:05 +0800

    分割符号改成\t

--- src/main/scala-2.10/run/Run.scala
+++ src/main/scala-2.10/run/Run.scala
@@ -24,7 +24,7 @@ object Run {
     val input = master + args(0) //"/hzl/input/cros3.csv" //输入文件
     //将输入每一行数据转成向量Dense(x,y)
     val dataRDD = SparkObj.ctx.textFile(input).map(s => {
-        Vectors.dense(s.split(',').map(_.toDouble))
+        Vectors.dense(s.split('\t').map(_.toDouble))
       }).cache()
     //    val clusRDD = KM.kmeans(dataRDD).predict(dataRDD).zip(dataRDD).persist()
     ConfigKM.totalDataCount = dataRDD.count()
@@ -41,7 +41,7 @@ object Run {
     val outputFS = new Path(output)
     // 删除输出目录
     if (hdfs.exists(outputFS)) hdfs.delete(outputFS, true)
-    //    clus.repartition(1).saveAsTextFile(output)
+    clus.repartition(1).saveAsTextFile(output)
     //    clus.saveAsTextFile(output)
     //save hdfs
 
@@ -56,16 +56,11 @@ object Run {
       //转成字符串返回，带固定结果
       //shffle会需大量时间
       MatrixUtil.matrix2String(SKPC.extractPC(dataMat), "reduce")
-    })
-    //      .reduce(MatrixUtil.matrixPlus2String(_, _))
-    //    //输出结果
-    //    //    println("the cluster result is:")
-    //    //    println(acc.value)
-    //    println("The original lines are:")
-    //    println(allLines)
-    //    println("The linked lines are:")
-    //    println(VectorUtil.vector2String(
-    //      PCUtil.linkLines(MatrixUtil.matrixStr2Matrix(allLines))))
-    allLines.saveAsTextFile(output)
+    }).reduce(MatrixUtil.matrixPlus2String(_, _))
+    //输出结果
+    println(allLines)
+    println(VectorUtil.vector2String(
+      PCUtil.linkLines(MatrixUtil.matrixStr2Matrix(allLines))))
+    //    allLines.saveAsTextFile(output)
   }
 }
