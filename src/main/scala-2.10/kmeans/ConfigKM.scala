463a6145c294322c2355fc86e41efccefc639705
huzuoliang <huzuoliang1990@163.com>
2017-07-20 14:57:59 +0800

    Revert "修复粒化代码 如果一个粒的话会导致结果为0"
    This reverts commit e93c4bd15ba64a1e6490c2971f09b1a2ac8bdf9d.

--- src/main/scala-2.10/kmeans/ConfigKM.scala
+++ src/main/scala-2.10/kmeans/ConfigKM.scala
@@ -11,12 +11,8 @@ object ConfigKM {
 
   val itersTimes: Int = 20 //聚类迭代次数
 
-  val reRunTimes: Int = 1
+  val reRunTimes: Int = 1 //重复运行次数。选择最优的
 
-  //重复运行次数。选择最优的
-  //类别数量
-  def classCount: Int = {
-    val count = Math.ceil(totalDataCount / classDataNum).toInt
-    if (count == 0) 1 else count
-  }
+  def classCount: Int = Math.ceil(totalDataCount / classDataNum).toInt //类别数量
+
 }
